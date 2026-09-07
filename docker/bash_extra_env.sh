2d36f1569bda28fa811b79bbb485f5c6ecb0edce
Remi <remi.poree@laas.fr>
2026-09-07 17:25:55 +0200

    kinda starting to work - move detection part in it's own docker

--- docker/bash_extra_env.sh
+++ docker/bash_extra_env.sh
@@ -6,6 +6,6 @@ export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:src/gazebo_procedural_world_generati
 
 # Set ROS 2 environment variables
 export ROS_DOMAIN_ID=65
-export ROS_LOCALHOST_ONLY=1
+# export ROS_LOCALHOST_ONLY=1 # deprecated + no need since already setup in tirrex
 
 exec bash
